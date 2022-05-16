13cdc2294bbcc5f505a1ed6c1f50d6a73d2c7c1a
yimvc <yimvc@amazon.com>
2022-05-16 12:49:07 -0700

    Service package on Service branch

---
+++ src/com/hotel/service/CustomerService.java
@@ -0,0 +1,36 @@
+package com.hotel.service;
+
+import com.hotel.resource.HotelResource;
+
+class CustomerService implements HotelResource {
+    @Override
+    public Customer getCustomer(String email) {
+        return null;
+    }
+
+    @Override
+    public void createACustomer(String email, String firstName, String lastName) {
+
+    }
+
+    @Override
+    public IRoom getRoom(String roomNumber) {
+        return null;
+    }
+
+    @Override
+    public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date CheckOutDate) {
+        return null;
+    }
+
+    @Override
+    public Collection<Reservation> getCustomersReservations(String customerEmail) {
+        return null;
+    }
+
+    @Override
+    public Collection<IRoom> findARoom(Date checkIn, Date checkOut) {
+        return null;
+    }
+
+}
