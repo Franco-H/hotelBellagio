a1d44f139c7abdb211fe50b2d812fcc71e05e2e2
JoeyHJava <3studyemail@gmail.com>
2022-05-17 08:10:06 -0700

    ENUM values RoomTypes

--- src/com/hotel/model/RoomType.java
+++ src/com/hotel/model/RoomType.java
@@ -1,8 +1,11 @@
 package com.hotel.model;
 
 enum RoomType {
-    // TODO: 5/16/2022: Identify types of rooms
-    PRESIDENTAL,
-    SUITE,
-    HIGHROLLER
+    RESORT_KING,
+    RESORT_TWO_QUEEN_ROOM,
+    FOUNTAIN_VIEW_KING,
+    FOUNTAIN_VIEW_TWO_QUEEN,
+    RESORT_TOWER_KING,
+    NEWLY_RENOVATED_PREMIER_TWO_QUEEN,
+    NEWLY_RENOVATED_PREMIER_KING,
 }
