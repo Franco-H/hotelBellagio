932276950fb8402e133c24680686e8f09ee2c326
yimvc <yimvc@amazon.com>
2022-05-17 08:44:27 -0700

    This is the services package.

---
+++ src/com/hotel/service/CustomerService.java
@@ -0,0 +1,73 @@
+package com.hotel.service;
+
+
+import com.hotel.model.Customer;
+import com.hotel.resource.HotelResource;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+class CustomerService {
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    Collection<Customer> customers = new ArrayList<>();
+
+    public CustomerService(String email, String firstName, String lastName) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void addCustomer(String email, String firstName, String lastName) {
+
+    }
+
+    public Customer getCustomer(String customerEmail) {
+        return null;
+    }
+
+
+    public Collection<Customer> getAllCustomer() {
+        return null;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerService{" +
+                "email='" + email +
+                ", firstName='" + firstName +
+                ", lastName='" + lastName +
+                ", customers=" + customers +
+                '}';
+    }
+}
+
+
+}
