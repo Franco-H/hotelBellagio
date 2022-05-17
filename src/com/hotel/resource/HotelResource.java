c310328a97b798357b6b67422d5464958ea24cab
yimvc <yimvc@amazon.com>
2022-05-17 08:26:51 -0700

    This is the resource package

---
+++ src/com/hotel/resource/HotelResource.java
@@ -0,0 +1,11 @@
+package com.hotel.resource;
+
+public interface HotelResource {
+    void getCustomer();
+    void createCustomer();
+    void getRoom();
+    void checkEmail();
+    void bookARoom();
+    void getCustomerReservation();
+    void findARoom();
+}
