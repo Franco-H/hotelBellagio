58a17680d27e5e7698fd5e9b7cca8e39d1fb0709
JoeyHJava <3studyemail@gmail.com>
2022-05-18 13:37:38 -0700

    CSV reader

--- src/com/hotel/menu/BellagioApp.java
+++ src/com/hotel/menu/BellagioApp.java
@@ -1,32 +1,47 @@
 package com.hotel.menu;
 
-import com.apps.util.SplashApp;
-import java.text.ParseException;
+//import com.apps.util.SplashApp;
+import com.hotel.model.Room;
+import com.hotel.model.RoomLoader;
 
-class BellagioApp implements SplashApp {
-    @Override
-    public void start() {
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+class BellagioApp {
+    public static void main(String[] args) throws IOException {
         Menu menu = new Menu();
-        try {
-            menu.execute();
-        } catch (ParseException e) {
+        RoomLoader roomLoader = new RoomLoader();
+        try{
+            Menu.execute();
+        }catch (ParseException e){
             e.printStackTrace();
         }
     }
+//    @Override
+//    public void start() {
+//        Menu menu = new Menu();
+//        try {
+//            menu.execute();
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-    @Override
-    public void welcome(String... strings) throws IllegalArgumentException {
-        SplashApp.super.welcome(strings);
-    }
+//    @Override
+//    public void welcome(String... strings) throws IllegalArgumentException {
+//        SplashApp.super.welcome(strings);
+//    }
+//
+//    @Override
+//    public void welcome(long l, String... strings) throws IllegalArgumentException {
+//        SplashApp.super.welcome(l, strings);
+//    }
 
-    @Override
-    public void welcome(long l, String... strings) throws IllegalArgumentException {
-        SplashApp.super.welcome(l, strings);
-    }
-
-    public static void main(String[] args) {
-        BellagioApp app = new BellagioApp();
-        app.welcome("images/credits.jpg", "images/bellagio.png");
-        app.start();
-    }
+//    public static void main(String[] args) {
+//        BellagioApp app = new BellagioApp();
+////        app.welcome("images/credits.jpg", "images/bellagio.png");
+////        app.start();
+//    }
 }
--- src/com/hotel/model/RoomLoader.java
+++ src/com/hotel/model/RoomLoader.java
@@ -1,5 +1,7 @@
 package com.hotel.model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,22 +12,22 @@ public class RoomLoader {
 
     private final Path roomsFilePath;
 
-    public RoomLoader(String roomsFilePath) {
-        this.roomsFilePath = Path.of(roomsFilePath);
+    public RoomLoader() {
+        this.roomsFilePath = Path.of("data/room-data.csv");
     }
 
-    public List<Room> getRooms() throws IOException{
+    public List<Room> getRooms() throws IOException {
         List<Room> result = new ArrayList<>();
 
         Files.lines(roomsFilePath).forEach(line -> {
             String[] tokens = line.split(",");
 
-            int roomNumber = Integer.parseInt(tokens[0]);
-            double price = Double.parseDouble(tokens[1]);
+            String roomNumber = tokens[0];
+            String price = tokens[1];
             RoomType roomType = RoomType.valueOf(tokens[2]);
-            String description = tokens[3];
-
-            result.add(new Room(roomNumber, price, roomType, description));
+//            String description = tokens[3];
+            boolean isFree = Boolean.parseBoolean(tokens[4]);
+            result.add(new Room(roomNumber, price, roomType, true));
         });
         return result;
     }
