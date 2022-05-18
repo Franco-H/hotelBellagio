1770c8bfa0e82b88e44cd936f6652722e1cddaf7
Franco-H <84954135+Franco-H@users.noreply.github.com>
2022-05-18 10:32:02 -0700

    Update CustomerService.java

--- src/com/hotel/service/CustomerService.java
+++ src/com/hotel/service/CustomerService.java
@@ -1,90 +1,37 @@
 package com.hotel.service;
 
 import com.hotel.model.Customer;
-import com.hotel.model.IRoom;
-import com.hotel.model.Reservation;
+import com.hotel.resource.HotelResource;
 
 import java.util.*;
 
+public class CustomerService {
+    private static final CustomerService CUSTOMER_SERVICE = new CustomerService();
+    private static Collection<Customer> customers;
 
-public class ReservationService {
-    Set<IRoom> rooms = new HashSet<>();
-    Set<Reservation> reservations = new HashSet<>();
-    public static ReservationService RESERVATION_SERVICE = new ReservationService();
-
-    public ReservationService() {
+    public CustomerService() {
     }
 
-    public static ReservationService getInstance() {
-        return RESERVATION_SERVICE;
+    public static CustomerService getInstance() {
+        return CUSTOMER_SERVICE;
     }
 
-    public void addRoom(IRoom room) {
-        rooms.add(room);
+    public void addCustomer(String email, String firstName, String lastName) {
+        customers.add(new Customer(firstName, lastName, email));
     }
 
-    public IRoom getRoom(String roomNumber) {
-        for (IRoom room : rooms) {
-            if (!room.getRoomNumber().equals(roomNumber)) {
-                continue;
+    public Customer getCustomer(String customerEmail) {
+        // return null if customerEmail is not found
+        for (Customer customer : customers) {
+            if (customer.getEmail().equals(customerEmail)) {
+                return customer;
             }
-            return room;
         }
         return null;
     }
 
-    public Reservation reserveRoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) {
-
-        Reservation reservation = new Reservation(customer, room, checkInDate, checkOutDate);
-        reservations.add(reservation);
-        return reservation;
+    public Collection<Customer> getAllCustomers() {
+        return customers;
     }
 
-    public Collection<IRoom> findRooms(Date checkIn, Date checkOut) {
-        if (reservations.isEmpty()) {
-            return rooms;
-        } else {
-            return findAvailableRooms(checkIn, checkOut);
-        }
-    }
-
-    private Collection<IRoom> findAvailableRooms(Date checkIn, Date checkOut) {
-        List<IRoom> availableRooms = new ArrayList<>();
-
-        for (Reservation reservation : reservations) {
-            if (!reservation.getCheckIn().after(checkIn) && !reservation.getCheckOut().before(checkOut)) {
-                for (IRoom room : rooms) {
-                    if (!reservation.getRoom().equals(room)) {
-                        availableRooms.add(room);
-                    }
-                }
-            }
-        }
-        return availableRooms;
-    }
-
-    public static Collection<IRoom> getAllRooms() {
-        return RESERVATION_SERVICE.rooms;
-    }
-
-    public Collection<Reservation> getCustomersReservation(Customer customer) {
-        Collection<Reservation> filteredReservations = new LinkedList<>();
-        for (Reservation reservation : reservations) {
-            if (reservation.getCustomer().equals(customer)) {
-                filteredReservations.add(reservation);
-            }
-        }
-        return filteredReservations;
-    }
-
-    public void displayAllReservations() {
-        if (reservations.isEmpty()) {
-            System.out.println("No reservations found");
-        } else {
-            for (Reservation reservation : reservations) {
-                System.out.println(reservation);
-            }
-        }
-    }
 }
-
